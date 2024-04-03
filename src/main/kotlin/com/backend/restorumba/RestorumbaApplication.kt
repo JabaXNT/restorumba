a1570bace38cf8d75c7158aaa495f737a815e6a8
glukhov.ilya@urfu.me <glukhov.ilya@urfu.me>
2024-04-03 15:24:28 +0500

    Kotlin Spring back created

---
+++ src/main/kotlin/com/backend/restorumba/RestorumbaApplication.kt
@@ -0,0 +1,11 @@
+package com.backend.restorumba
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestorumbaApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestorumbaApplication>(*args)
+}
---
+++ src/test/kotlin/com/backend/restorumba/RestorumbaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backend.restorumba
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestorumbaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
