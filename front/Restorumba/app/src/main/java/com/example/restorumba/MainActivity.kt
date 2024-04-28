088ff5ed7182cbeee42a0c112ae4303499228945
Kirill <alex.ujikol@mail.ru>
2024-04-28 17:40:19 +0500

    add project & layout

---
+++ front/Restorumba/app/src/main/java/com/example/restorumba/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.restorumba
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main_screen)
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.constraintLayout)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }*/
+    }
+}
