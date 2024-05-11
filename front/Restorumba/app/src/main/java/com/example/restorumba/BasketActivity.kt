814dbb1b271e9bc74b1c964f19568e2af2af2e81
Kirill <alex.ujikol@mail.ru>
2024-05-11 17:05:51 +0500

    some front sheeet

--- front/Restorumba/app/src/main/java/com/example/restorumba/BasketActivity.kt
+++ front/Restorumba/app/src/main/java/com/example/restorumba/BasketActivity.kt
@@ -1,6 +1,8 @@
 package com.example.restorumba
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,14 @@ class BasketActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val home: ImageButton = findViewById(R.id.home)
+
+        home.setOnClickListener {
+            val intent = Intent(this, OrderActivity::class.java)
+            startActivity(intent)
+            finish()
         }
     }
 }
--- front/Restorumba/app/src/main/java/com/example/restorumba/SettingsActivity.kt
+++ front/Restorumba/app/src/main/java/com/example/restorumba/SettingsActivity.kt
@@ -2,11 +2,18 @@ package com.example.restorumba
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+import java.lang.reflect.Modifier
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,11 +27,50 @@ class SettingsActivity : AppCompatActivity() {
         }
 
         val back: Button = findViewById(R.id.back)
+        val darkModeSwitch: androidx.appcompat.widget.SwitchCompat = findViewById(R.id.dark_mode_switch)
+        val vibrationSwitch: androidx.appcompat.widget.SwitchCompat = findViewById(R.id.vibration_switch)
+
+        val deleteButton: TextView = findViewById(R.id.delete_button)
+        val deleteMenu: ConstraintLayout = findViewById(R.id.delete_menu)
+        val deleteSubmit: TextView = findViewById(R.id.delete_submit)
+        val closeMenu: ImageButton = findViewById(R.id.close_menu)
+
+        darkModeSwitch.setOnClickListener {
+            Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show()
+        }
+
+        vibrationSwitch.setOnClickListener {
+            Toast.makeText(this, "TODO", Toast.LENGTH_SHORT).show()
+        }
 
         back.setOnClickListener {
             val intent = Intent(this, ProfileActivity::class.java)
             startActivity(intent)
             finish()
+        }
+
+        deleteButton.setOnClickListener {
+            deleteMenu.visibility = View.VISIBLE
+            darkModeSwitch.isEnabled = false
+            vibrationSwitch.isEnabled = false
+            back.isEnabled = false
+        }
+
+        deleteSubmit.setOnClickListener {
+            deleteMenu.visibility = View.GONE
+            darkModeSwitch.isEnabled = true
+            vibrationSwitch.isEnabled = true
+            back.isEnabled = true
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        closeMenu.setOnClickListener {
+            deleteMenu.visibility = View.GONE
+            darkModeSwitch.isEnabled = true
+            vibrationSwitch.isEnabled = true
+            back.isEnabled = true
         }
     }
 }
